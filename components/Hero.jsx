4b2913f8673b061e9db803d3f8f744daed2a8fcf
Eduardo Ribeiro <eduardo.ribeiro@outlook.pt>
2021-06-30 02:09:34 +0100

    Add icons and change services and hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,44 +1,44 @@
-import React, { useState, useEffect } from "react";
-import { Jumbotron, Container, Row, Col } from "reactstrap";
+import React, { useState, useEffect } from 'react';
+import { Jumbotron, Container, Row, Col } from 'reactstrap';
 
 const slides = [
   {
     id: 1,
-    service: "Ar Condicionado",
+    service: "Ar Condicionados",
     image: "ac.jpg",
   },
   {
     id: 2,
-    service: "Frigorifico",
+    service: "Frigorificos",
     image: "frigorifico.jpg",
   },
   {
       id: 3,
       isFemale: true,
-    service: "Máquina de Lavar",
+    service: "Máquinas de Lavar",
     image: "washer.jpeg",
   },
   {
       id: 4,
       isFemale: true,
-    service: "Máquina da Loiça",
+    service: "Máquinas da Loiça",
     image: "dishwasher.jpeg",
   },
   {
       id: 5,
       isFemale: true,
-    service: "Caldeira",
+    service: "Caldeiras",
     image: "hotboiler.jpeg",
   },
   {
       id: 6,
       isFemale: true,
-    service: "Caldeira Industrial",
+    service: "Caldeiras Industriais",
     image: "electric-boiler.jpeg",
   },
   {
       id: 7,
-    service: "Esquentador",
+    service: "Esquentadores",
     image: "gas-boiler.jpeg",
   },
 ];
@@ -59,14 +59,15 @@ const Hero = () => {
       style={{
         background: `url(/images/${slide.image}) no-repeat center fixed`,
         backgroundSize: "cover",
-        transition: "backgroud .2s ease-in-out"
+        transition: "backgroud .2s ease-in-out",
+        height: '90vh',
       }}
     >
       <Jumbotron fluid>
         <Container>
           <Row className="align-items-center">
             <Col
-              lg={4}
+              lg={5}
               style={{
                 background: "rgba(255,255,255, 0.75)",
                 borderRadius: "5px",
@@ -75,10 +76,10 @@ const Hero = () => {
             >
               <div className="pr-lg-5">
                 <p className="text-uppercase text-primary font-weight-medium f-14 mb-4">
-                  Reparações 24/7
+                  Serviço 24/7
                 </p>
                 <h1 className="mb-4 font-weight-normal line-height-1_4">
-                  Problemas com {slide.isFemale ? "a sua": "o seu"}{" "}
+                  Instalação, manutenção e reparação de{" "}
                   <p className="text-primary font-weight-medium" style={{transition: "all .2s ease-in-out"}}>
                     {slide.service}
                   </p>
--- components/Service.jsx
+++ components/Service.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { Container, Row, Col } from "reactstrap";
 const Service = () => {
   const services = [
-    { title : "Ar Condicionados", desc : "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s" },
-    { title : "Caldeiras", desc : "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s" },
-    { title : "Esquentadores", desc : "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s" },
-    { title : "Frigoríficos", desc : "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s" },
-    { title : "Frio Industrial", desc : "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s" },
-    { title: "Bombas de Calor", desc: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s" },
-    { title: "Aquecimento Central", desc: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s" },
-    { title: "Energia Solar", desc: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s" },
-    { title : "Ventilação", desc : "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s" },
+    { title: 'Ar Condicionados', desc : '', icon: '' },
+    { title: 'Caldeiras', desc : '', icon: '' },
+    { title: 'Esquentadores', desc : '', icon: '' },
+    { title: 'Frigoríficos', desc : '', icon: '' },
+    { title: 'Frio Industrial', desc : '', icon: '' },
+    { title: 'Bombas de Calor', desc: '', icon: '' },
+    { title: 'Aquecimento Central', desc: '', icon: '' },
+    { title: 'Energia Solar', desc: '', icon: '' },
+    { title: 'Ventilação', desc : '', icon: '' },
   ]
   
   return (
@@ -20,14 +20,14 @@ const Service = () => {
           <Col lg={6} md={8}>
             <div className="title text-center mb-5">
               <h3 className="font-weight-normal text-dark"><span className="text-warning">Serviços</span></h3>
-              <p className="text-muted">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+              <p className="text-muted">Instalação, manutenção e reparação especializada de:</p>
             </div>
           </Col>
         </Row>
         <Row>
           {
             services.map((service, key) =>
-              <Col key={key} lg={4} md={6}>
+              <Col key={key} lg={3} md={6}>
                 <div>
                   <div className="mb-5">
                       <i className={service.icon}></i>
